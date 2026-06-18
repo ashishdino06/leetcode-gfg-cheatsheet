60a88ba6c52236171b8ce3898fbe4971d0bf0c1a
Ashish maurya <ashishdino06@gmail.com>
2026-06-18 23:03:06 +0530

    Update stringProblems.py

--- string/stringProblems.py
+++ string/stringProblems.py
@@ -1,1 +1,9 @@
 
+import re
+from collections import Counter
+paragraph = "a b.b"
+banned=[]
+banned_set=set(banned)
+word=re.findall(r'[A-Za-z]+',paragraph)
+counts=Counter(w.lower() for w in word if w.lower() not in banned_set)
+print(counts.most_common(1)[0][0])
