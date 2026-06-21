5c2cbbb96813025d839c2f0d804a27cad368f4fc
Ashish maurya <ashishdino06@gmail.com>
2026-06-21 12:50:16 +0530

    Update Floor_in_sortedArr.py
    Finding the value from an array such that it is <= to target

--- array/Floor_in_sortedArr.py
+++ array/Floor_in_sortedArr.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def findFloor(self, arr, x):
+        # code here
+        ans=-1
+        l,h=0,len(arr)-1
+        while l<=h:
+            mid= l + (h-l)//2
+            
+            if arr[mid]>x:
+                h=mid-1
+            else:
+                ans = mid
+                l = mid+1
+        return ans
