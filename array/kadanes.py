8ac068ed8d4818526207cefc2e3680b8c4f1fb99
Ashish maurya <ashishdino06@gmail.com>
2026-06-21 12:48:34 +0530

    Update kadanes.py
    Solving for maximum subarray sum using Kadane's algo

--- array/kadanes.py
+++ array/kadanes.py
@@ -1,1 +1,13 @@
+def maxSubarraySum(arr):
+  max_sum_so_far=arr[0]
+  curr_max = arr[0]
+  for i in range(1,len(arr)):
+    print(f"At {i}th ",curr_max,i)
+    print(f"At {i}th ",max_sum_so_far,i)
+    curr_max = max(arr[i],curr_max+arr[i])
+    max_sum_so_far = max(max_sum_so_far,curr_max)
+    print(curr_max)
+    print(max_sum_so_far)
 
+arr = [2, 3, -8, 7, -1, 2, 3]
+maxSubarraySum(arr)
